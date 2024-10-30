caac4cf71c415cd1261213028dbd3ae443ccdcdd
Wictor Zhao <wictor_x@hotmail.com>
2024-10-30 13:47:17 +0100

    test

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,7 +1,11 @@
-import type { ServerBuild } from "@remix-run/cloudflare";
 import { createRequestHandler } from "@remix-run/cloudflare";
 import * as build from "../build/server";
 
-export const onRequest = createRequestHandler(
-  build as unknown as ServerBuild
-);
+export const onRequest = createRequestHandler({
+  build,
+  mode: process.env.NODE_ENV ?? "development",
+  getLoadContext: (context: any) => ({
+    env: context.env,
+    cloudflare: context
+  })
+});
