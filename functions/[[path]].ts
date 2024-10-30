85b4af1bb4b5a2c08b965067b181da918ccc3d85
Wictor Zhao <wictor_x@hotmail.com>
2024-10-30 13:44:47 +0100

    change path

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,11 +1,7 @@
+import type { ServerBuild } from "@remix-run/cloudflare";
 import { createRequestHandler } from "@remix-run/cloudflare";
 import * as build from "../build/server";
 
-export const onRequest = createRequestHandler({
-  build,
-  mode: process.env.NODE_ENV ?? "development",
-  getLoadContext: (context) => ({
-    env: context.env,
-    cloudflare: context
-  })
-});
+export const onRequest = createRequestHandler(
+  build as unknown as ServerBuild
+);
