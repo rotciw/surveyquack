c081786d90a788812bb1f57f99de545edb0188c7
Wictor Zhao <wictor_x@hotmail.com>
2024-10-30 13:48:17 +0100

    wrangler

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,10 +1,11 @@
+import type { ServerBuild } from "@remix-run/cloudflare";
 import { createRequestHandler } from "@remix-run/cloudflare";
 import * as build from "../build/server";
 
 export const onRequest = createRequestHandler({
-  build,
+  build: build as unknown as ServerBuild,
   mode: process.env.NODE_ENV ?? "development",
-  getLoadContext: (context: any) => ({
+  getLoadContext: (context) => ({
     env: context.env,
     cloudflare: context
   })
